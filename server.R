60e517baf7731f516fa5d9827ced897e2380d9c9
Roberto Gonzalo Rodriguez <gonzarodri@gmail.com>
2015-09-25 11:27:56 -0300

    App scafolding

--- server.R
+++ server.R
@@ -17,7 +17,25 @@ shinyServer(function(input, output) {
     
     # draw the histogram with the specified number of bins
     hist(x, breaks = bins, col = 'darkgray', border = 'white')
+
+  })
+  
+  output$ziptable <- DT::renderDataTable({
+
+    df <- cleantable
     
+#     df <- cleantable %>%
+#       filter(
+#         Score >= input$minScore,
+#         Score <= input$maxScore,
+#         is.null(input$states) | State %in% input$states,
+#         is.null(input$cities) | City %in% input$cities,
+#         is.null(input$zipcodes) | Zipcode %in% input$zipcodes
+#       ) %>%
+#       mutate(Action = paste('<a class="go-map" href="" data-lat="', Lat, '" data-long="', Long, '" data-zip="', Zipcode, '"><i class="fa fa-crosshairs"></i></a>', sep=""))
+#     action <- DT::dataTableAjax(session, df)
+    
+    DT::datatable(df, escape = FALSE)
   })
   
 })
--- ui.R
+++ ui.R
@@ -4,25 +4,92 @@
 # 
 # http://www.rstudio.com/shiny/
 #
+#
 
 library(shiny)
+library(leaflet)
 
-shinyUI(pageWithSidebar(
-  
-  # Application title
-  headerPanel("Old Faithful Geyser Data"),
-  
-  # Sidebar with a slider input for number of bins
-  sidebarPanel(
-    sliderInput("bins",
-                "Number of bins:",
-                min = 1,
-                max = 50,
-                value = 30)
-  ),
-  
-  # Show a plot of the generated distribution
-  mainPanel(
-    plotOutput("distPlot")
+shinyUI(
+  navbarPage("Tab1", id="nav",
+    tabPanel("TabPanel",
+             pageWithSidebar(
+               
+               # Application title
+               headerPanel("Old Faithful Geyser Data"),
+               
+               # Sidebar with a slider input for number of bins
+               sidebarPanel(
+                 sliderInput("bins",
+                             "Number of bins:",
+                             min = 1,
+                             max = 50,
+                             value = 30)
+               ),
+               
+               # Show a plot of the generated distribution
+               mainPanel(
+                 plotOutput("distPlot")
+               )
+             )
+             
+             
+             
+             
+             
+             
+             ),
+    tabPanel("TabPanel2"
+             ,
+             fluidRow(
+               column(3,
+                      selectInput("states", "States", c("All states"="", structure(state.abb, names=state.name), "Washington, DC"="DC"), multiple=TRUE)
+               ),
+               column(3,
+                      conditionalPanel("input.states",
+                                       selectInput("cities", "Cities", c("All cities"=""), multiple=TRUE)
+                      )
+               ),
+               column(3,
+                      conditionalPanel("input.states",
+                                       selectInput("zipcodes", "Zipcodes", c("All zipcodes"=""), multiple=TRUE)
+                      )
+               )
+             ),
+             fluidRow(
+               column(1,
+                      numericInput("minScore", "Min score", min=0, max=100, value=0)
+               ),
+               column(1,
+                      numericInput("maxScore", "Max score", min=0, max=100, value=100)
+               )
+             ),
+             hr(),
+             DT::dataTableOutput("ziptable")
+             
+             )
   )
-))
+  
+    
+
+
+#   pageWithSidebar(
+#   
+#   # Application title
+#   headerPanel("Old Faithful Geyser Data"),
+#   
+#   # Sidebar with a slider input for number of bins
+#   sidebarPanel(
+#     sliderInput("bins",
+#                 "Number of bins:",
+#                 min = 1,
+#                 max = 50,
+#                 value = 30)
+#   ),
+#   
+#   # Show a plot of the generated distribution
+#   mainPanel(
+#     plotOutput("distPlot")
+#   )
+# )
+
+)
